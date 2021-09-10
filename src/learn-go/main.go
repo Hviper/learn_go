a352c00ae50b5312713245790c664ac900b23afe
hdc <1467520438@qq.com>
2021-09-10 14:09:49 +0800

    commit first

--- src/learn-go/main.go
+++ src/learn-go/main.go
@@ -1,9 +1,49 @@
 package main
 
-import  "fmt" 
+import (
+	"fmt"
+)
+
+
+func delp() int {
+	
+	return 1
+}
+
+// func function_name(input1 type1, input2 type2) (type1, type2) {
+// 	函数体
+// 	返回多个值
+// 	return value1, value2
+//  }
+
+func add(a int,b int) int{
+	numbers := make(map[string]int)
+	numbers["name"] = 15
+	numbers["age"] = 15
+	fmt.Println(numbers["name"])
+	delete(numbers,"name")
+	fmt.Println("这是水水水水水水-->",numbers)
+	return a+b
+}
+
+func apache(a *int){
+	*a +=1
+}
 func main(){
-	fmt.Println("hello,world")
-	for i := 0; i < 15; i++ {
-		fmt.Println(i)
+	arr := [] string{"aaa","bbb"}
+	test(arr)
+	fmt.Println(arr)
+
+}
+func forEach(arr [] int){
+	for k,v:=range arr{
+		fmt.Println(k,v)
 	}
 }
+
+func test(arr [] string) ([] string){
+	arr = append(arr,"hahha")
+	return arr
+}
+
+

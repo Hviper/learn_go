5a5a371ed9abfb4c42348893a8d8332139fd9d39
hdc <1467520438@qq.com>
2021-09-14 23:20:44 +0800

    gin - router

--- src/learn-gin/main.go
+++ src/learn-gin/main.go
@@ -2,19 +2,73 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 )
 
-func handle(c ...interface{}) {
-	fmt.Println(c)
+func channelTest() {
+	wg := sync.WaitGroup{}
+	chanel1 := make(chan int, 1)
+	chanel2 := make(chan int, 1)
+	wg.Add(2)
+	go func() {
+		chanel1 <- 15
+		chanel2 <- 15
+		chanel1 <- 15
+		chanel2 <- 15
+		wg.Done()
+	}()
+	go func() {
+		flag := true
+		for flag {
+			select {
+			case i := <-chanel1:
+				fmt.Println(i)
+			case i := <-chanel2:
+				fmt.Println(i)
+			default:
+				{
+					flag = false
+				}
+			}
+
+		}
+		wg.Done()
+	}()
+	wg.Wait()
 }
-func main() {
+
+//动态参数,
+func handle(c *gin.Context) {
+	listData := []string{
+		"北京",
+		"天安门",
+		"南京",
+		"天津大金矿",
+		"天津大麻花",
+	}
+	data := map[string]interface{}{
+		"name": "kiwi",
+		"age":  18,
+		"list": listData,
+	}
+	c.JSON(200, data)
+}
+func applicationRun() {
 	router := gin.Default()
+
 	router.GET("/ping", func(c *gin.Context) {
+		names := c.QueryArray("name")
+
 		c.JSON(200, gin.H{
 			"message": "pong",
+			"names":   names,
 		})
-	})
+	}, handle)
 	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+func main() {
+	applicationRun()
+
 }
